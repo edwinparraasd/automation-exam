85c67a7f25a3df2c4d4350c2d6532460af33a391
David Peña <davidpena@Davids-MacBook-Pro.local>
2020-01-08 12:52:47 -0500

    Order and print the products

--- features/automation-exam/step_definitions/step_validations.rb
+++ features/automation-exam/step_definitions/step_validations.rb
@@ -11,26 +11,23 @@ Then('Order by price ascendant') do
 end
 
 Then('Assert the order taking the first {int} results') do |total_asserts|
-  items = @river_results.river_result_container.all('li', class: 's-item')
-  @data_results = Array.new
-
-  items.each do |item|
-    name = item.all('h3', class: 's-item__title').first.text
-    price = Helper.convert_price(item.all('span', class: 's-item__price').first.text)
-    shipping = Helper.convert_shipping(item.all('span', class: 's-item__shipping').first.text)
-    @data_results << [name, price + shipping]
-  end
+  @data_results = Helper.data_search(@river_results, 'Array')
 
   @data_results.each_with_index do |data, index|
-    break if index.eql?  total_asserts.to_i
-    expect(data[1]).to be <= @data_results[index+1][1]
+    break if index.eql? total_asserts.to_i
+
+    expect(data[1]).to be <= @data_results[index + 1][1]
   end
 end
 
 Then('Order and print the products by name in ascendant mode') do
-  byebug
+  data_results = Helper.data_search(@river_results, 'Array of Hashes')
+  data_results.sort_by! { |data| data[:name] }
+  Helper.print_results(data_results)
 end
 
 Then('Order and print the products by price in descendant mode') do
-  pending # Write code here that turns the phrase above into concrete actions
+  data_results = Helper.data_search(@river_results, 'Array of Hashes')
+  data_results.sort_by! { |data| -data[:full_price] }
+  Helper.print_results(data_results)
 end
