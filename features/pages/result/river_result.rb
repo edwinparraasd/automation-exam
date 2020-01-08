07e18c7195ec0be9b82645eba7a97ebe2aef3eac
David Peña <davidpena@Davids-MacBook-Pro.local>
2020-01-07 23:22:05 -0500

    Refining the Search by Branch and Size

--- features/pages/result/river_result.rb
+++ features/pages/result/river_result.rb
@@ -2,24 +2,40 @@
 
 module Pages
   module Result
-    # In this class there are all the elements of Index page
+    # In this class there are all the elements of River Results page
     # and its main actions
     class RiverResult < SitePrism::Page
-      element :search_field, '#gh-ac'
+      element :search_brand_field, '.x-searchable-list__textbox__aspect-Brand'
+      element :brand_container, '#x-refine__group_1__1'
+      element :size_container, '#x-refine__group_1__0'
 
-      def go_home_page
-        visit 'https://www.ebay.com/'
-      end
-
-      def search_keyword(keyword)
-        search_keyword_input(keyword)
+      def refining_search_by(searchable, keyword)
+        case searchable
+        when 'Brand'
+          refining_search_by_brand(keyword)
+        when 'US Shoe Size'
+          refining_search_by_size(keyword)
+        end
       end
 
       private
 
-      def search_keyword_input(keyword)
-        wait_until_search_field_visible
-        search_field.set keyword
+      def refining_search_by_brand(keyword)
+        wait_until_search_brand_field_visible
+        search_brand_field.set keyword
+        select_brand(keyword)
+      end
+
+      def select_brand(keyword)
+        wait_until_brand_container_visible
+        items = brand_container.all('span', exact_text: keyword)
+        items.first.click
+      end
+
+      def refining_search_by_size(keyword)
+        wait_until_size_container_visible
+        items = size_container.all('span', exact_text: keyword)
+        items.first.click
       end
     end
   end
