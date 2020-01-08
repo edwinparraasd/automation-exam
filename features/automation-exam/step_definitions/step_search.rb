d9e2f6e670cb9268de73534e71aad6163ba11d3a
David Peña <david.pena@youse.com.co>
2020-01-07 23:22:52 -0500

    Refining the Search by Branch and Size (#4)

--- features/automation-exam/step_definitions/step_search.rb
+++ features/automation-exam/step_definitions/step_search.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
-When('Search Brand {string}') do |_string|
-  pending # Write code here that turns the phrase above into concrete actions
+When('Search Brand {string}') do |keyword|
+  @river_results = Pages::Result::RiverResult.new
+  @river_results.refining_search_by('Brand', keyword)
 end
 
-When('Select size {string}') do |_string|
-  pending # Write code here that turns the phrase above into concrete actions
+When('Select size {string}') do |keyword|
+  @river_results = Pages::Result::RiverResult.new
+  @river_results.refining_search_by('US Shoe Size', keyword)
 end
